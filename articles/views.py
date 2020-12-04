a84d8bd703be4b84afa8a38cccb5e941d206395d
Johannes Ferreira <johannesssf@gmail.com>
2020-12-04 17:19:28 -0300

    Add login redirect

--- articles/views.py
+++ articles/views.py
@@ -9,23 +9,27 @@ from .models import Article
 class ArticleListView(LoginRequiredMixin, ListView):
     model = Article
     template_name = 'article_list.html'
+    login_url = 'login'
 
 
 class ArticleDetailView(LoginRequiredMixin, DetailView):
     model = Article
     template_name = 'article_detail.html'
+    login_url = 'login'
 
 
 class ArticleUpdateView(LoginRequiredMixin, UpdateView):
     model = Article
     fields = ['title', 'body']
     template_name = 'article_edit.html'
+    login_url = 'login'
 
 
 class ArticleDeleteView(LoginRequiredMixin, DeleteView):
     model = Article
     template_name = 'article_delete.html'
     success_url = reverse_lazy('article_list')
+    login_url = 'login'
 
 
 class ArticleCreateView(LoginRequiredMixin, CreateView):
